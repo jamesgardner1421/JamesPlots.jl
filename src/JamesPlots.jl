81a941e0489c7f78365b17b2071d26752de41418
James Gardner <james.gardner1421@gmail.com>
2022-03-17 14:20:48 +0000

    Update theme

--- src/JamesPlots.jl
+++ src/JamesPlots.jl
@@ -2,13 +2,18 @@
 module JamesPlots
 
 using CairoMakie
+using ColorSchemes
+using Colors
 
+# https://github.com/OrdnanceSurvey/GeoDataViz-Toolkit/tree/master/Colours
+const COLORS = ColorScheme(parse.(Colorant, ["#ff1f5b", "#00cd6c", "#009ade", "#af58ba", "#ffc61e", "#f28522"]))
 const COLUMN_WIDTH = 240
 const TWO_COLUMN_WIDTH = 481
 const RESOLUTION = (COLUMN_WIDTH, COLUMN_WIDTH/MathConstants.golden)
 
 export MyAxis
 export save_figure
+export COLORS
 
 function MyAxis(f; kwargs...)
     my_ax = Axis(f; kwargs...)
@@ -22,8 +27,6 @@ function MyAxis(f; kwargs...)
 end
 
 function choose_color(ncolors)
-    # https://github.com/OrdnanceSurvey/GeoDataViz-Toolkit/tree/master/Colours
-    colors = ["#ff1f5b", "#00cd6c", "#009ade", "#af58ba", "#ffc61e", "#f28522"]
 
     selectors = [
         [1],
@@ -34,19 +37,19 @@ function choose_color(ncolors)
         [1, 2, 3, 4, 5, 6],
     ]
     select = selectors[ncolors]
-    return [colors[i] for i in select]
+    return [COLORS[i] for i in select]
 end
 
-function get_theme(width; ncolors=5)
+function get_theme(;scale, ncolors)
 
     color = choose_color(ncolors)
 
     Theme(
-        resolution=RESOLUTION .* width,
+        resolution=RESOLUTION .* scale,
         figure_padding=1,
         rowgap=0,
         colgap=0,
-        font="Times New Roman", fontsize=8 * width,
+        font="Times New Roman", fontsize=8 * scale,
         Axis=(
             xminorticksvisible=true,
             yminorticksvisible=true,
@@ -56,37 +59,43 @@ function get_theme(width; ncolors=5)
             ytickalign=1,
             xminortickalign=1,
             yminortickalign=1,
-            spinewidth=width,
-            xtickwidth=width,
-            xminortickwidth=width,
-            ytickwidth=width,
-            yminortickwidth=width,
-            xminorticksize=2width,
-            xticksize=3width,
-            yminorticksize=2width,
-            yticksize=3width,
+            spinewidth=scale,
+            xtickwidth=scale,
+            xminortickwidth=scale,
+            ytickwidth=scale,
+            yminortickwidth=scale,
+            xminorticksize=2scale,
+            xticksize=3scale,
+            yminorticksize=2scale,
+            yticksize=3scale,
+            xlabelpadding=scale,
+            ylabelpadding=scale,
         ),
         palette=(patchcolor=collect(color), color=color),
-        Lines = (
-            linewidth=1.5width,
+        Lines=(
+            linewidth=1.5scale,
         ),
-        Scatter =(
-            strokewidth=width,
-            markersize=4width
+        Scatter=(
+            strokewidth=scale,
+            markersize=4scale
         ),
+        Legend=(
+            framevisible=true,
+            colgap=5scale,
+            rowgap=0,
+            patchsize=(10scale, 10scale),
+            patchlabelgap=3scale,
+            padding=(3scale, 3scale, 2scale, 2scale),
+            merge=true,
+            labelsize=8scale,
+    )
     )
 end
 
-const PUBLICATION_THEME = get_theme(1)
-const VIEW_THEME = get_theme(2)
-
-function __init__()
-    set_theme!(VIEW_THEME)
-end
-
-function save_figure(file, plot_function)
-    with_theme(PUBLICATION_THEME) do 
-        save(file, plot_function(); pt_per_unit=1)
+function save_figure(file, plot_function; scale=1, ncolors=3)
+    theme = get_theme(;scale, ncolors)
+    with_theme(theme) do 
+        save(file, plot_function(); px_per_unit=4, pt_per_unit=1)
     end
 end
 
